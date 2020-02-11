a62b15036d0310b914ac3662c163c211a9cc47d7
motoras <motoras@gmail.com>
2020-02-11 19:14:38 +0200

    Fix module export issue.

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,9 @@ pub mod core {
     pub use kekbit_core::api::*;
     pub use kekbit_core::header::*;
     pub use kekbit_core::shm::reader::ShmReader;
+    pub use kekbit_core::shm::shm_reader;
+    pub use kekbit_core::shm::shm_writer;
+    pub use kekbit_core::shm::storage_path;
     pub use kekbit_core::shm::writer::ShmWriter;
-    pub use kekbit_core::shm::*;
     pub use kekbit_core::tick::*;
 }
